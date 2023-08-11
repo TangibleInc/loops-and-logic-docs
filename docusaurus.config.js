e79a245e75b23e66d240c27dd12d30afbca95ca2
Tangimark <mark@teamtangible.com>
2023-08-11 08:27:42 -0400

    Switch the sitemap over to proper URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://docusaurus-loops-and-logics.pages.dev',
+  url: 'https://docs.loopsandlogic.com'
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
