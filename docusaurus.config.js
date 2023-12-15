e3af728b4ca17fc9d49ad859af50fe37559f8866
eliot-akira <me@eliotakira.com>
2023-12-15 17:45:51 +0100

    Add redirect to /reference/framework

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,20 @@ const config = {
     locales: ['en'],
   },
 
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/reference/template-system/framework',
+            to: '/reference/framework'
+          }
+        ]
+      }
+    ]
+  ],
+
   presets: [
     [
       'classic',
